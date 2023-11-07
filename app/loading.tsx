f7120e7540de226be89300e77df84b6f481f5a7f
kinglemar <dimeji.ogunleye20@gmail.com>
2023-11-07 18:29:41 +0100

    Route preloader.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,40 +1,5 @@
-"use client";
-import BrandLogo from "./components/BrandLogo";
-import { useRef, useLayoutEffect } from "react";
-
-import { gsap } from "@/app/lib/utils";
+import PagePreloader from "./components/PagePreloader";
 
 export default function Loading() {
-  const logo = useRef<HTMLDivElement | null>(null);
-    
-  useLayoutEffect(() => {
-    let ctx = gsap.fromTo(
-      logo.current,
-      {
-        scale: 0.8,
-        duration: 4,
-        animationDelay: 3,
-        ease: "elastic",
-        yoyo: true,
-      },
-      {
-        scale: 2,
-        opacity: 1,
-        duration: 4,
-        animationDelay: 3,
-        ease: "elastic",
-        yoyo: true,
-      }
-    );
-    return () => {
-      ctx.kill();
-    };
-  }, []);
-  return (
-    <section className="w-screen h-screen overflow-hidden flex flex-col justify-center items-center bg-black">
-      <div className="w-fit" ref={logo}>
-        <BrandLogo />
-      </div>
-    </section>
-  );
+  return <PagePreloader />;
 }
