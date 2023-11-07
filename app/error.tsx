5b9fd83c32f46651b636944c0a0c98218327c1c0
kinglemar <dimeji.ogunleye20@gmail.com>
2023-11-07 18:17:52 +0100

    Error boundary component.

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect } from "react";
+import BrandLogo from "./components/BrandLogo";
 
 export default function Error({
   error,
@@ -13,14 +14,15 @@ export default function Error({
   }, [error]);
 
   return (
-    <div className="w-fit mx-auto">
-        <pre>An error occured. Please refresh</pre>
-        <button
-          className="px-5 py-2 my-5 m-3 w-fit bg-[#76c893] rounded-md shadow-md"
-          onClick={() => reset()}
-        >
-          Try again
-        </button>
-      </div>
+    <div className="w-screen h-screen overflow-hidden flex flex-col justify-center items-center bg-white">
+      <BrandLogo/>
+      <pre className="mt-10">An error occured. Please refresh</pre>
+      <button
+        className="px-5 py-2 my-5 m-3 w-fit text-white bg-blue-200 rounded-md shadow-md"
+        onClick={() => reset()}
+      >
+        Try again
+      </button>
+    </div>
   );
 }
